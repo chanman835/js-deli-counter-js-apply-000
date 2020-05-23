a06c6295617cc0be5249740b37a181b7471d9182
Winson Chan <chanw835@gmail.com>
2020-05-23 23:51:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function takeANumber(line,person) {
   line.push(person);
   var place = 0;
-  return `Welcome ${person}. You are number ${place++} in line.
+  return `Welcome ${person}. You are number ${place++} in line.`
 }
   
 
