e67d64f67460b25a17d6670c9252c7ca76435b92
Winson Chan <chanw835@gmail.com>
2020-05-24 00:26:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ return string with person's name and index of person, + 1
 */ 
 
 function nowServing(katzDeliLine) {
-  if (katzDeliLine === []; return 'There is nobody waiting to be served!'
+  if (katzDeliLine === []); return 'There is nobody waiting to be served!'
 }
 
 /* base case -if deli line is empty array, return the string
