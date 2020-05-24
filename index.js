ca9daccffbac176c4a2ad2529663cfcaf610859f
Winson Chan <chanw835@gmail.com>
2020-05-24 00:15:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ function nowServing(katzDeliLine) {
   if (!katzDeliLine) return 'There is nobody waiting to be served!'
   else {
     let person = katzDeliLine[0];
-    return katzDeliLine.push(person).pop(person)
+    return katzDeliLine.pop(person)
     console.log(katzDeliLine)
   }
 }
 
 /* base case -if deli line is empty array, return the string
-return 
+pop the element at index 0 of deli line */
