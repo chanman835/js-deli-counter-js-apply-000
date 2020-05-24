0bf6c3b2a6b4cd9872b06c6d64afa7cfe3aa4bc9
Winson Chan <chanw835@gmail.com>
2020-05-24 00:00:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 function takeANumber(katzDeliLine,person) {
-  katzDeliLine.push(person);
   var place = 0;
+  for (let i = 0; i < person.length; i++) {
+    katzDeliLine.push(person[i]);
+    place++
+  }
   
-  return `Welcome, ${person}. You are number ${place += 1} in line.`
+  return `Welcome, ${person}. You are number ${place} in line.`
 }
   
 
 
-/* push person name into katzDeliLine
+/* iterate through each person 
+push person name into katzDeliLine
 assign place in line of person, originally 0
 return string with person's name and incremented place in line 
 */ 
